2573af5fa9120aae1fe1fb3c08ac7b492522160f
tengfei2015 <xitengfei@aragoncs.com>
2018-07-08 22:50:43 +0800

    practice

--- TicTacToe/src/components/Game.js
+++ TicTacToe/src/components/Game.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import '../css/Game.css'
 
-class Square extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			value: null,
-		}
-	}
-	render() {
-		return (
-			<button className="square" onClick={()=> this.setState({value:'X'})} >
-			{this.state.value}
-			</button>
-		);
-	}
+
+function Square(props) {
+	return (
+		<button
+			className="square"
+			onClick={ props.onClick }
+		>
+			{props.value}
+		</button>
+	);
 }
 
 class Board extends Component{
@@ -22,15 +18,40 @@ class Board extends Component{
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		};
 	}
 
+	handleClick(i){
+		const squares = this.state.squares.slice();
+		if(caculateWinner(squares) || squares[i]){
+			return
+		}
+		squares[i] = this.state.xIsNext ? 'X':'O';
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
 	renderSquare(i) {
-		return <Square value={i} />;
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 
 	render() {
-		const status = "Next Player: X";
+		const winner = caculateWinner(this.state.squares)
+		let status
+		if(winner){
+            status = winner + " is Win, Congratulations!"
+		}else{
+            status = "Next Player: " + (this.state.xIsNext?'X':'O')
+		}
+
 		return (
 			<div>
 				<div className="status">{status}</div>
@@ -53,6 +74,28 @@ class Board extends Component{
 		);
 	}
 }
+
+
+function caculateWinner(squares){
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+    for(var i=0;i<lines.length;i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a]; //squares[a] is win
+        }
+    }
+    return null;
+}
+
 
 class Game extends Component{
 	render() {
